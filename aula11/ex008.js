1b59b9a9d3e5794d71bc3a42a61354465464b5e9
ARJUNIORAR <141863248+ARJUNIORAR@users.noreply.github.com>
2025-03-28 11:53:33 -0300

    atualizar 008js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,7 @@
-console.log('O console executou corretamente')
+var vel = 12.5
+console.log (`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) {
+    console.log (`Voce ultrapssou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre com segurança!`)
+
