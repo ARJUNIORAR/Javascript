e7de753b463c1d9ab5df3f6dafa8690f1a735167
ARJUNIORAR <141863248+ARJUNIORAR@users.noreply.github.com>
2025-04-03 17:38:41 -0300

    verficador de img

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,50 @@
+function verificar() {
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.getElementById('res')
+  if (fano.value.length == 0 || Number(fano.value) > ano ) {
+    window.alert('[ERRO] Verifiquei os dados e tente novamente !')
+  } else {
+    var fsex = document.getElementsByName ('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+        genero = 'Homem'
+        if (idade >=0 && idade <10) {
+            //criança
+            img.setAttribute('src', 'imagem/bebeM.jpg')
+        }   else if (idade <21){
+            //jovem
+            img.setAttribute('src', 'imagem/jovemM.jpg')
+        } else if ( idade < 50){
+            //Adulto
+            img.setAttribute('src', 'imagem/homem.jpg')
+        } else {
+            //idoso
+            img.setAttribute('src', 'imagem/idosoM.jpg')
+        }
+    } else if (fsex[1].checked){
+        genero = 'mulher'
+        if (idade >=0 && idade <10) {
+            //criança
+            img.setAttribute('src', 'imagem/bebef.jpg')
+        }   else if (idade <21){
+            //jovem
+            img.setAttribute('src', 'imagem/jovemF.jpg')
+        } else if ( idade < 50){
+            //Adulto
+            img.setAttribute('src', 'imagem/mulher.jpg')
+        } else {
+            //idoso
+            img.setAttribute('src', 'imagem/idosoF.jpg')
+        }
+    }
+    res.style.textAlign = 'center' 
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
+    
+  }
+}
