0f8d8ad5d68f37bf03db00daf09c79bfc47bf227
ARJUNIORAR <141863248+ARJUNIORAR@users.noreply.github.com>
2025-04-15 17:32:46 -0300

    while

---
+++ aula13/ambiente.js
@@ -0,0 +1,5 @@
+var c = 1
+while (c <= 6) {
+    console.log("Tudo bem")
+    c ++ // essa linha quer dizer que C vai passar a valer 2, porque recebe C = 1, Assim a estrutura de repetiçã retorna com 3 e ainda será menhor que 6, voltando a repetir até o a variavel ser maior que 6 e a estrutura parar de repetir.
+}
